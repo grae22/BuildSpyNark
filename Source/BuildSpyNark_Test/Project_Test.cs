94fde5b4d9ec5001c59694fae287080eb686ff3d
GraemeB <grae22@gmail.com>
2016-10-15 14:41:53 +0200

    Adding test project.

---
+++ Source/BuildSpyNark_Test/Project_Test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BuildSpyNark_Test
+{
+  [TestClass]
+  public class Project_Test
+  {
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+  }
+}
